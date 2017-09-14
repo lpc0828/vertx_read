603cf8ef040dea8b64ce8e24191d1900a43b265a
pcliu <liupengcheng@daling.com>
2017-09-14 11:10:55 +0800

    增加测试代码

--- src/main/java/com/pengcheng/verxt/Launcher.java
+++ src/main/java/com/pengcheng/verxt/Launcher.java
@@ -2,10 +2,81 @@ package com.pengcheng.verxt;
 
 import com.pengcheng.verxt.instances.MyFirstVerticle;
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
+
+import java.lang.management.*;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Launcher {
 
     public static void main(String[] args) {
         Vertx.vertx().deployVerticle(MyFirstVerticle.class.getName());
+        /*Vertx.vertx().deployVerticle(MyFirstVerticle.class.getName());*/
+//        System.out.println("=========================== 1111111111 ==================================");
+//        printThreadInfo();
+//        WebClientOptions options = new WebClientOptions();
+//        Vertx vertx = Vertx.vertx(new VertxOptions());
+//        WebClient client = WebClient.create(vertx, options.setMaxPoolSize(Runtime.getRuntime().availableProcessors()*64));
+//        System.out.println("=========================== 2222222222 ==================================");
+//        printThreadInfo();
+//        for (int i=0; i<10; i++) {
+//            client.post(8081, "127.0.0.1", "/xc_sale/uc/auth.do")
+//                    .putHeader("platform", "android")
+//                    .putHeader("utoken", "10364121-17237-8a17328e-9f7b-4042-a5b5-66efd5508420")
+//                    .addQueryParam("trackId", String.valueOf(i))
+//                    .send(res -> {
+//                        if (res.succeeded()) {
+//                            System.out.println(res.result().bodyAsString());
+//                        } else if (res.failed()) {
+//                            System.out.println("httpRequest Error!");
+//                        }
+//                    });
+//        }
+//        System.out.println("send complete!");
+//        try {
+//            Thread.sleep(10000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println("=========================== 2222222222 ==================================");
+//        printThreadInfo();
+//        client.close();
+
+    }
+
+
+    public static void printThreadInfo() {
+        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+        long[] threadIds = threadMXBean.getAllThreadIds();
+        Map<String, ThreadInfo> liveThreadMap = new TreeMap<String, ThreadInfo>();
+        for (long threadId : threadIds) {
+            // 线程的信息
+            ThreadInfo threadInfo = threadMXBean.getThreadInfo(threadId);
+            // 线程被阻塞的数量
+            threadInfo.getBlockedCount();
+            // 被锁定线程的监控信息
+            MonitorInfo[] monitorInfos = threadInfo.getLockedMonitors();
+            for (MonitorInfo monitorInfo : monitorInfos) {
+                int depth = monitorInfo.getLockedStackDepth();
+                System.out.println("锁定程度：" + depth);
+            }
+            // 异步锁定的信息
+            LockInfo[] lockinfos = threadInfo.getLockedSynchronizers();
+            // 锁定的信息
+            for (LockInfo lockInfo : lockinfos) {
+                System.out.println("锁定类名称：" + lockInfo.getClassName());
+            }
+
+            liveThreadMap.put(threadInfo.getThreadName(), threadInfo);
+
+        }
+        for(Map.Entry<String, ThreadInfo> entry : liveThreadMap.entrySet()){
+            String threadName = entry.getKey();
+            ThreadInfo threadInfo = entry.getValue();
+            System.out.println("线程名称：" + threadName + ",状态[" + threadInfo.getThreadState().name() + "]");
+        }
     }
 }
--- src/main/java/com/pengcheng/verxt/instances/MyFirstVerticle.java
+++ src/main/java/com/pengcheng/verxt/instances/MyFirstVerticle.java
@@ -1,14 +1,23 @@
 package com.pengcheng.verxt.instances;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpServer;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MyFirstVerticle extends AbstractVerticle {
 
     int i =0;
+    AtomicInteger index = new AtomicInteger(0);
 
     @Override
     public void start() {
         vertx.createHttpServer().requestHandler(req -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             String threadName = genThreadName();
             System.out.println(threadName+" 开始执行。。。。。。");
             long start = System.currentTimeMillis();
@@ -23,11 +32,37 @@ public class MyFirstVerticle extends AbstractVerticle {
             i++;
             System.out.println(threadName+" 执行完毕。。。。。。i="+i + ", cost:"+(System.currentTimeMillis()-start));
         }).listen(8083);
+
+
+
+        vertx.createHttpServer().requestHandler(req -> {
+            String threadName = genThreadName();
+            System.out.println(threadName+" 开始执行。。。。。。");
+            vertx.executeBlocking(future->{
+                String threadName1 = genThreadName();
+                System.out.println(threadName1+" async 开始执行。。。。。。");
+                int value = index.incrementAndGet();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                future.complete(value);
+            }, false, res->{
+                String threadName1 = genThreadName();
+                System.out.println(threadName1+" res 开始执行。。。。。。");
+                if (res.succeeded()) {
+                    System.out.println(res.result() + " ["+System.currentTimeMillis()+"] complete......");
+                }
+                req.response().end();
+            });
+        }).listen(8084);
+
     }
 
 
     private String genThreadName() {
         String threadName = Thread.currentThread().getName();
-        return "[" + threadName + "]";
+        return "["+ System.currentTimeMillis()/1000L + "]"+"[" + threadName + "]";
     }
 }
